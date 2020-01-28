52ffc01656df5eec7148450154bfeaa6c8be13d1
Tom <Tora02001@utb.vaxjo.se>
2020-01-28 20:36:31 +0100

    InsertionSort

--- Sorting v2/Sorting v2/Program.cs
+++ Sorting v2/Sorting v2/Program.cs
@@ -1,37 +1,18 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace BubbleSort
+namespace InsertionSort
 {
     class Program
     {
         static void Main(string[] args)
         {
             Stopwatch sw = new Stopwatch();
-            int[] bubble = randomNumbers();
+            int[] InsertionSort = randomNumbers();
             sw.Start();
-            Bubbelsortera(bubble);
+            Insertion(InsertionSort);
             sw.Stop();
             Console.WriteLine(sw.Elapsed.TotalSeconds+" Sekunder tog det!");
-        }
-        public static void Bubbelsortera(int[] lista)
-        {
-            for (int i = lista.Length - 1; i > 0; i--)
-            {
-                for (int n = 0; n < i; n++)
-                {
-                    if (lista[n] > lista[n + 1])
-                    {
-                        int temp = lista[n];
-                        lista[n] = lista[n + 1];
-                        lista[n + 1] = temp;
-                    }
-                }
-            }
-            for (int i = 0; i < lista.Length; i++)
-            {
-                Console.WriteLine(lista[i]);
-            }
         }
         public static int[] randomNumbers()
         {
@@ -59,6 +40,10 @@ namespace BubbleSort
                     a--;
                 }
                 lista[a + 1] = temp;
+            }
+            for (int i = 0; i < lista.Length; i++)
+            {
+                Console.WriteLine(lista[i]);
             }
         }
     }
